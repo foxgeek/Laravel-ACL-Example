1df3fbd80d6eceab126cd3855922ec52bfc8a647
webmaster@iopago.com.br <hellish.doom@gmail.com>
2017-11-15 14:19:04 -0200

    Adicionado postagens ao index do sistema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,19 +12,6 @@
 
         <!-- Styles -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
             .flex-center {
                 align-items: center;
                 display: flex;
@@ -41,27 +28,7 @@
                 top: 18px;
             }
 
-            .content {
-                text-align: center;
-            }
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
@@ -69,7 +36,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">Suas postagens</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
@@ -78,16 +45,27 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            @forelse($posts as $post)
+                                <h2>{{ $post->title }}</h2>
+                                <p>
+                                    {{ $post->body }}
+                                </p>
+                                <strong style="display: block;">Autor: {{ $post->user->name }}</strong>
+                                @can('update-post', $post)
+                                    <a href="{{ route('posts.edit', ['id'=> $post->id]) }}" class="btn btn-primary">Editar Post</a>
+                                @endcan
+                                <hr>
+                            @empty
+                                <h3>Nenhum post encontrado</h3>
+                                <p>Para criar os posts use a PostFactory.</p>
+                            @endforelse
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
