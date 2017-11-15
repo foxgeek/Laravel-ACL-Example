171dc075eba3075ca54e7767d675ebc104f1b42d
webmaster@iopago.com.br <hellish.doom@gmail.com>
2017-11-15 12:36:52 -0200

    Add view com listagem de posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-12">
+            @forelse($posts as $post)
+                <h2>{{ $post->title }}</h2>
+                <p>
+                    {{ $post->body }}
+                </p>
+                <strong>Autor: {{ $post->user->name }}</strong>
+                <hr>
+            @empty
+                <h3>Nenhum post encontrado</h3>
+                <p>Para criar os posts use a PostFactory.</p>
+            @endforelse
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
         </div>
     </div>
 </div>
